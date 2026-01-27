544bad5abee844f3b1857ccfc4c0cc1c29e81172
condre-art <condre@rosstaxprepandbookkeeping.com>
2026-01-27 00:36:54 -0600

    Add CORS middleware function

---
+++ src/src/src/routes/src/routes/src/middleware/cors.ts
@@ -0,0 +1,11 @@
+export function cors(res: Response) {
+  const headers = new Headers(res.headers);
+  headers.set("Access-Control-Allow-Origin", "*");
+  headers.set("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  headers.set("Access-Control-Allow-Headers", "Content-Type,Authorization");
+
+  return new Response(res.body, {
+    status: res.status,
+    headers
+  });
+}
